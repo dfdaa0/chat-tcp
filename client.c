463d47428b1e89b50da2c3b1568e8e8b305d72f8
Daniel Araújo <dfdaa0@outlook.com>
2023-07-02 16:51:57 -0300

    Update client.c

--- client.c
+++ client.c
@@ -8,6 +8,61 @@
 
 #define BUFFER_SIZE 1024
 
+void printErrorMessage(int code) {
+    switch (code) {
+        case 1:
+            printf("User limit exceeded\n");
+            break;
+        case 2:
+            printf("User not found\n");
+            break;
+        case 3:
+            printf("Receiver not found\n");
+            break;
+        default:
+            printf("Unknown error\n");
+            break;
+    }
+}
+
+void handleMessage(char *message) {
+    if (strncmp(message, "RES_LIST", 8) == 0) {
+        // Parse the user IDs from the message
+        char *list = strtok(message, "()");
+        char *ids = strtok(NULL, "()");
+        printf("Available users: %s\n", ids);
+    } else if (strncmp(message, "MSG", 3) == 0) {
+        // Parse the sender ID and message from the message
+        char *sender = strtok(message, "()");
+        char *msg = strtok(NULL, "()");
+        printf("%s: %s\n", sender, msg);
+    } else if (strncmp(message, "REQ_REM", 7) == 0) {
+        // Parse the user ID to be removed
+        char *userId = strtok(message, "()");
+        printf("User %s has been removed from the network\n", userId);
+    } else if (strncmp(message, "ERROR", 5) == 0) {
+        // Parse the receiver ID and error code from the message
+        char *receiver = strtok(message, "()");
+        char *errorCode = strtok(NULL, "()");
+        int code = atoi(errorCode);
+        printf("Error message for user %s: ", receiver);
+        printErrorMessage(code);
+    } else if (strncmp(message, "OK", 2) == 0) {
+        // Parse the receiver ID and success code from the message
+        char *receiver = strtok(message, "()");
+        char *successCode = strtok(NULL, "()");
+        int code = atoi(successCode);
+        printf("Success message for user %s: ", receiver);
+        if (code == 1) {
+            printf("Removed Successfully\n");
+        } else {
+            printf("Unknown code\n");
+        }
+    } else {
+        printf("Unknown message received from the server\n");
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("Usage: %s <server IP> <port>\n", argv[0]);
@@ -17,7 +72,7 @@ int main(int argc, char *argv[]) {
     int clientSocket;
     struct sockaddr_storage serverAddr;
     socklen_t addrSize;
-    char buffer[BUFFER_SIZE] = "REQ_ADD";
+    char buffer[BUFFER_SIZE];
 
     // Create socket
     if (strstr(argv[1], ":") != NULL) {
@@ -56,17 +111,17 @@ int main(int argc, char *argv[]) {
     printf("Connected to the server\n");
 
     // Send the initial request message
-    if (write(clientSocket, buffer, strlen(buffer)) < 0) {
-        perror("Write failed");
+    if (send(clientSocket, "REQ_ADD", 7, 0) < 0) {
+        perror("Send failed");
         exit(EXIT_FAILURE);
     }
 
-    // Send and receive messages
+    // Receive and send messages
     while (1) {
         // Receive message from the server
         memset(buffer, 0, BUFFER_SIZE);
-        if (read(clientSocket, buffer, BUFFER_SIZE) < 0) {
-            perror("Read failed");
+        if (recv(clientSocket, buffer, BUFFER_SIZE, 0) < 0) {
+            perror("Receive failed");
             exit(EXIT_FAILURE);
         }
 
@@ -78,18 +133,47 @@ int main(int argc, char *argv[]) {
             break;
         }
 
+        // Process the received message
+        handleMessage(buffer);
+
         // Clear the buffer
         memset(buffer, 0, BUFFER_SIZE);
 
-        printf("Enter a message: ");
+        // Get user input
+        printf("Enter a command: ");
         fgets(buffer, BUFFER_SIZE, stdin);
 
         // Remove newline character
         buffer[strcspn(buffer, "\n")] = '\0';
 
+        // Process user command
+        if (strcmp(buffer, "list users") == 0) {
+            // Request the list of user IDs from the server
+            sprintf(buffer, "RES_LIST");
+        } else if (strncmp(buffer, "send to", 7) == 0) {
+            // Parse the receiver ID and message from the command
+            int receiverId;
+            char *message;
+            sscanf(buffer, "send to %d \"%[^\"]\"", &receiverId, message);
+            // Send message to the server
+            sprintf(buffer, "MSG(%d,%d,%s)", receiverId, receiverId, message);
+        } else if (strncmp(buffer, "send all", 8) == 0) {
+            // Parse the message from the command
+            char *message;
+            sscanf(buffer, "send all \"%[^\"]\"", message);
+            // Send message to the server
+            sprintf(buffer, "MSG(%d,NULL,%s)", receiverId, message);
+        } else if (strcmp(buffer, "close connection") == 0) {
+            // Request disconnection from the server
+            sprintf(buffer, "REQ_REM(%d)", 0);  // Replace 0 with the actual user ID
+        } else {
+            printf("Unknown command\n");
+            continue;
+        }
+
         // Send message to the server
-        if (write(clientSocket, buffer, strlen(buffer)) < 0) {
-            perror("Write failed");
+        if (send(clientSocket, buffer, strlen(buffer), 0) < 0) {
+            perror("Send failed");
             exit(EXIT_FAILURE);
         }
     }
